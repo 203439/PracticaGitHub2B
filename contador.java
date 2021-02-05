9aad5c87b157f7c903ef5fab7f9bc212d138dfbb
203439 <203439@ids.upchiapas.edu.mx>
2021-02-05 12:33:56 -0600

    Primera version

---
+++ contador.java
@@ -0,0 +1,11 @@
+public class contador
+{
+    public static void main(String args[])
+    {
+        for(int i = 0; i <= 8; i++)
+        {
+            System.out.print("12 * "+ i + " = " + 12 * i + "\n");
+        }
+    }
+    
+}
